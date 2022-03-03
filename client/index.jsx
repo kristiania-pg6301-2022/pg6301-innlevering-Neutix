c90383c492089d91043860597812ff8a066823e4
Neutix <martin.wedvik@gmail.com>
2022-03-03 16:16:20 +0100

    added prettier concurrently and husky

---
+++ client/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+  useNavigate,
+} from "react-router-dom";
+
+function FrontPage() {
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <Link to="/question">
+        <button type="button">Start quiz</button>
+      </Link>
+    </div>
+  );
+}
+
+function App() {
+  return (
+                <BrowserRouter>
+             <Routes>
+            <Route path={"/"} element={<FrontPage />} />
+        </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
