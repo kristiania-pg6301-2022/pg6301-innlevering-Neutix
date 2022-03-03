a7289bf27c6041773db93e7b8553d4d0612b86a8
Neutix <martin.wedvik@gmail.com>
2022-03-04 00:09:42 +0100

    added question fetch

--- client/index.jsx
+++ client/index.jsx
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {
-  Routes,
-  Route,
-  Link,
-  BrowserRouter,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
-  return (
-    <div>
-      <h1>Quiz</h1>
-      <Link to="/question">
-        <button type="button">Start quiz</button>
-      </Link>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Code Quiz</h1>
+            <ul>
+                <li>
+                    <Link to="/answer">Answer a question</Link>
+                </li>
+                <li>
+                    <Link to="/score">Show score</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function AnswerQuestion() {
+    function handleConfirm(answer) {
+        fetch(`/questions/${question.id}/answer`, {
+            method: "post",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ answer }),
+        });
+    }
+
+    const [question, setQuestion] = useState();
+    useEffect(async () => {
+        const res = await fetch("/question/random");
+        setQuestion(await res.json());
+    }, []);
+
+    if (!question) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>{question.question}</h1>
+            {Object.keys(question.answers)
+                .filter((a) => question.answers[a])
+                .map((answer) => (
+                    <div key={answer}>
+                        <button onClick={(q) => handleConfirm("test")}>
+                            {question.answers[answer]}
+                        </button>
+                    </div>
+                ))}
+        </div>
+    );
+}
+
+function ShowScore() {
+    return <h1>Show score</h1>;
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage />} />
+                <Route path={"/answer"} element={<AnswerQuestion />} />
+                <Route path={"/score"} element={<ShowScore />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -6,47 +6,20 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(bodyParser.json());
-
-app.get("/api/question/random", (req, res) => {
+app.get("/question/random", (req, res) => {
     const question = randomQuestion();
-    res.json(question)
-    /*
-    console.log({id, question, answers, category})
-    res.json({id, question, answers, category})
-
-     */
-})
-/*
-app.post("/quiz/answer", (req, res) => {
-    const {id, answer} = req.body;
-    const question = Questions.find((q) => q.id === id);
-    if (!question) {
-        return res.sendStatus(404)
-    }
-    if (isCorrectAnswer(question, answer)) {
-        return res.json({result: "correct"})
-    }else {
-        return res.json({ result: "incorrect"})
-    }
-})
-
- */
-
-app.post("/api/question", (req, res) => {
-
-})
+    res.json(question);
+});
 
 app.use(express.static(path.resolve("../client/dist")));
 app.use((req, res, next) => {
-  if (req.method === "GET" && !req.path.startsWith("/api")) {
-    return res.sendFile(path.resolve("../client/dist/index.html"));
-  } else {
-    next();
-  }
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        return res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`server started on http://localhost:${server.address().port}`),
-    console.log(`questions on http://localhost:${server.address().port}/api/question`)
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`server started on http://localhost:${server.address().port}`);
 });
