53bab98732a6d9d6c3891087eda28ee6185ab82a
Neutix <72227602+Neutix@users.noreply.github.com>
2022-03-06 14:47:11 +0100

    Martin (#2)
    * make it better
    
    * new update
    
    * added question fetch
    
    * added buttons
    
    * added new name
    
    * update to package.json
    
    * browser finito test

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ https://innlevering-jess-martin.herokuapp.com/
 
 * [x] Få en Parcel til å bygge en React applikasjon
 * [x] Få React Router til å navigere rundt i applikasjonen
-* [ ] Få React til å hente og lagre informasjon til et API
+* [x] Få React til å hente og lagre informasjon til et API
 * [ ] Få Github Actions til å kjøre Jest-testene og publisere coverage til Coveralls
-* [ ] Få Heroku til å publisere websidene
+* [x] Få Heroku til å publisere websidene
+
--- client/index.jsx
+++ client/index.jsx
@@ -1,32 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  Routes,
-  Route,
-  Link,
-  BrowserRouter,
-  useNavigate,
-} from "react-router-dom";
+import {QuizApp} from "./quiz";
 
-function FrontPage() {
-  return (
-    <div>
-      <h1>Quiz</h1>
-      <Link to="/question">
-        <button type="button">Start quiz</button>
-      </Link>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<QuizApp />, document.getElementById("app"));
