d35288e7fb36210a845a81dc94544ce922d211f4
Neutix <martin.wedvik@gmail.com>
2022-03-06 17:44:27 +0100

    last commit

--- client/__test__/quiz.test.jsx
+++ client/__test__/quiz.test.jsx
@@ -1,16 +1,39 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import {QuizApp} from "../quiz.jsx";
+import {QuizApp, AnswerQuestion} from "../quiz.jsx";
 
-describe("Quiz test", () => {
 
-    it("DummyTest", () =>{
+const getQuestion = () => ({
+    id:1,
+    question:
+        "What is 4 + 4",
+    answers: {
+        answer_a: "140",
+        answer_b: "20",
+        answer_c: "56",
+        answer_d: "8",
+        answer_e: "10",
+        answer_f: "1000",
+    },
+})
+
+
+describe("Quiz application", () => {
+  it("DummyTest", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(<QuizApp />, element);
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+
+
+    it ("submit answer", () => {
+        const onSubmitAnswer = jest.fn();
+
         const element = document.createElement("div");
-        ReactDOM.render(<QuizApp />, element);
-        expect(element.innerHTML).toMatchSnapshot()
-    })
+        ReactDOM.render(<AnswerQuestion onSubmitAnswer={onSubmitAnswer} question={getQuestion()}/>, element);
 
-    }
+        expect(onSubmitAnswer).toHaveBeenCalled()
 
+    });
 
-    )
+});
